9bce13dc380ebba5eabe6335c26c7cd75357807f
Brandon Dooley <brandondooleycoding@gmail.com>
2025-07-26 16:58:24 -0700

    07262025

--- app.js
+++ app.js
@@ -25,17 +25,18 @@ const { HttpError } = require("./utils/errors");
 const mainRouter = require("./routes/index");
 const primaryRouter = require("./routes/primary");
 
-
 const corsOptions = {
   origin(origin, callback) {
-    if (
-      origin === "https://www.bigredcoding.com" ||
-      origin === "https://api.bigredcoding.com" ||
-      origin === "https://wtwr.bigredcoding.com" ||
-      origin === "https://planetpledge.bigredcoding.com" ||
-      origin === "https://finalproject.bigredcoding.com" ||
-      origin === "https://securitydemo.bigredcoding.com"
-    ) {
+    console.log("Request origin:", origin);
+    const allowed = [
+      "https://www.bigredcoding.com",
+      "https://api.bigredcoding.com",
+      "https://wtwr.bigredcoding.com",
+      "https://planetpledge.bigredcoding.com",
+      "https://finalproject.bigredcoding.com",
+      "https://securitydemo.bigredcoding.com",
+    ];
+    if (!origin || allowed.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("CORS not allowed"), false);
@@ -45,9 +46,10 @@ const corsOptions = {
   credentials: true,
 };
 
-
 const corsPolicy =
   process.env.NODE_ENV === "production" ? cors(corsOptions) : cors();
+
+app.options("*", corsPolicy);
 
 app.use(corsPolicy);
 
@@ -55,15 +57,16 @@ app.use(rateLimiter);
 
 app.use(requestLogger);
 
-mongoose.connect("mongodb://127.0.0.1:27017/1066843f2099e10caeb65d8e4165805e").catch(() => {
-  throw new HttpError.ServerError("An error has occurred on the server");
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/1066843f2099e10caeb65d8e4165805e")
+  .catch(() => {
+    throw new HttpError.ServerError("An error has occurred on the server");
+  });
 
 app.use(express.json());
 
 app.use("/projects", rateLimiter, mainRouter);
 app.use("/primary", rateLimiter, primaryRouter);
-
 
 app.listen(PORT);
 
