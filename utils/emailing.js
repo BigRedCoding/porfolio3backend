0b5db3f24d3025ef4daff816dab7f11b3c765e0c
BigRedCoding <brandondooleycoding@gmail.com>
2026-08-01 15:49:15 -0700

    08-01-2026

--- utils/emailing.js
+++ utils/emailing.js
@@ -3,14 +3,13 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE === "true",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-    },
+  host: process.env.BREVO_SMTP_HOST,
+  port: Number(process.env.BREVO_SMTP_PORT),
+  secure: false,
+  auth: {
+    user: process.env.BREVO_SMTP_USER,
+    pass: process.env.BREVO_SMTP_PASS,
+  },
 });
-
 
 module.exports = transporter;
